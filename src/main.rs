f4a196921512e9adc2d764991c986831ddb32d9d
Neokoldstudio <nightmare.vid@gmail.com>
2025-01-21 01:45:41 -0500

    exposed the knobs

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,61 @@
 use std::time::SystemTime;
 
 use image::{open, GrayImage, ImageBuffer, Luma, RgbaImage};
-use noise::{NoiseFn, Perlin, Seedable};
+use noise::{NoiseFn, Perlin, ScaleBias, Seedable};
 use rand::Rng;
 
 fn main() {
-    // génération d'image à partir de bruit de perlin
-    let thresholds = vec![0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9];
+    //==================VARIABLES==================
+    let layers = 50;
+    let img_width = 1920;
+    let img_height = 1080;
+    let scale_x = 0.1;
+    let scale_y = 0.1;
+    let low_thresh = 0;
+    let high_thresh = 220;
+    let background = [0, 0, 0];
+    //=============================================
 
-    let img = generate_perlin_image(1080, 1920, thresholds);
-    img.save("perlin.png").expect("Failed to save image");
-    // chargement de l'image
-    let img = open("input.png")
-        .expect("Failed to open image")
-        .into_rgba8();
+    // génération d'image à partir de bruit de perlin
+    let thresholds: Vec<f64> = (0..layers).map(|i| i as f64 / layers as f64).collect();
+
+    let img = generate_perlin_image(
+        img_height, img_width, thresholds, scale_x, scale_y, background,
+    );
+    img.save("output/perlin.png").expect("Failed to save image");
+    // chargement de l'image -> décommenter pour appliquer le tri de pixel sur une image existante
+    // let img = open("input.png")
+    //     .expect("Failed to open image")
+    //     .into_rgba8();
 
     // grayscale
     let gray_img = convert_to_luminance(&img);
 
     // masque de luminance, permet de conserver la structure de l'image lors du tri.
-    let (low_thresh, high_thresh) = (50, 150); //seuil de luminance pour le masque, varible permettant de contrôler le rendu final
     let mask = create_luminance_mask(&gray_img, low_thresh, high_thresh);
 
-    mask.save("mask.png").expect("Failed to save image"); //-->FOR DEBUG
-                                                          //tri des pixels selon le masque de luminance (blanc = 255->pixel a trier, noir = 0->ne pas trier), les pixels sont triés par bandes continues de pixels blancs.
+    //mask.save("output/mask.png").expect("Failed to save image"); //-->FOR DEBUG
+    //tri des pixels selon le masque de luminance (blanc = 255->pixel a trier, noir = 0->ne pas trier), les pixels sont triés par bandes continues de pixels blancs.
     let sorted_img = sort_image_using_mask(&img, &mask);
 
-    sorted_img.save("output.png").expect("Failed to save image");
+    sorted_img
+        .save("output/output.png")
+        .expect("Failed to save image");
 }
 
-fn generate_perlin_image(height: i32, width: i32, thresholds: Vec<f64>) -> RgbaImage {
+fn generate_perlin_image(
+    height: i32,
+    width: i32,
+    thresholds: Vec<f64>,
+    scale_x: f64,
+    scale_y: f64,
+    background: [u8; 3],
+) -> RgbaImage {
     let mut rng = rand::thread_rng();
-    let colors: Vec<[u8; 3]> = (0..thresholds.len())
+    let mut colors: Vec<[u8; 3]> = (0..thresholds.len()) //génération de couleurs aléatoires pour chaque seuil, pourrait bénéficier d'une amélioration en générant une palette de couleur harmonieuse
         .map(|_| [rng.gen(), rng.gen(), rng.gen()])
         .collect();
-
+    colors[0] = background; //couleur du fond
     let perlin = Perlin::new(
         SystemTime::now()
             .duration_since(SystemTime::UNIX_EPOCH)
@@ -45,8 +66,8 @@ fn generate_perlin_image(height: i32, width: i32, thresholds: Vec<f64>) -> RgbaI
 
     for (x, y, pixel) in img.enumerate_pixels_mut() {
         let value = perlin.get([
-            x as f64 / (width as f64 * 0.2),
-            y as f64 / (height as f64 * 0.2),
+            x as f64 / (width as f64 * scale_x),
+            y as f64 / (height as f64 * scale_y),
         ]);
         let index = thresholds
             .iter()
